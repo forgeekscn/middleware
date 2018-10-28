7b43a1a3cbacee215f1ca2c2a81a2f6db4e61561
forgeekscn <forgeekscn@gmail.com>
2018-10-28 20:59:41 +0800

    SYSTEM : Remove ELK.

--- src/main/java/cn/forgeeks/springcloud/elastic/controller/ElkLogController.java
+++ src/main/java/cn/forgeeks/springcloud/elastic/controller/ElkLogController.java
@@ -1,4 +1,4 @@
-package cn.forgeeks.springcloud.elk.controller;
+package cn.forgeeks.springcloud.elastic.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/cn/forgeeks/springcloud/fastDFS/MainApplication.java
+++ src/main/java/cn/forgeeks/springcloud/fastDFS/MainApplication.java
@@ -1,14 +1,13 @@
-package cn.forgeeks.springcloud.elk;
+package cn.forgeeks.springcloud.fastDFS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Init ELK run-env.
- */
 @SpringBootApplication
-public class MainApplication{
+public class MainApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
+
 }
