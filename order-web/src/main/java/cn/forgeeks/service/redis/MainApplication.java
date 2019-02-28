f06022243422eca8a1888e4030ebe74de8c3776d
hechao <forgeekscn@gmail.com>
2019-02-28 20:19:41 +0800

    redis service

--- order-web/src/main/java/cn/forgeeks/service/redis/MainApplication.java
+++ order-web/src/main/java/cn/forgeeks/service/redis/MainApplication.java
@@ -1,4 +1,4 @@
-package cn.forgeeks.order.web;
+package cn.forgeeks.service.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
