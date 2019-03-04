4e93eacb59409b3466c616f398803988942c77e3
hechao <forgeekscn@gmail.com>
2019-03-05 00:10:45 +0800

    language review

--- mq-service/src/main/java/cn/forgeeks/service/common/common/RabbitMailSender.java
+++ mq-service/src/main/java/cn/forgeeks/service/common/common/RabbitMailSender.java
@@ -1,16 +1,14 @@
 package cn.forgeeks.service.common.common;
 
 import cn.forgeeks.service.common.dto.MessageDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class RabbitMailSender {
-
-    private static final Logger log = LoggerFactory.getLogger(RabbitMailSender.class);
 
     @Autowired
     RabbitTemplate rabbitTemplate;
@@ -18,9 +16,9 @@ public class RabbitMailSender {
     /**
      * 生产邮件消息
      */
-    public void sendMail(MessageDto mail){
-        rabbitTemplate.convertAndSend(Consts.EXCHANGE_MYTH_TOPIC  ,Consts.QUEUE_MYTH_MAIL_ALL , mail );
-        log.info("邮件生产者[{}]",mail);
+    public void sendMail(MessageDto mail) {
+        rabbitTemplate.convertAndSend(Consts.EXCHANGE_MYTH_TOPIC, Consts.QUEUE_MYTH_MAIL_ALL, mail);
+        log.info("邮件生产者[{}]", mail);
     }
 
 }
---
+++ utils-service/src/main/java/cn/forgeeks/service/common/testclz/Clz2.java
@@ -0,0 +1,20 @@
+package cn.forgeeks.service.common.testclz;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Clz2 extends Clz1 {
+    static {
+        log.info("clz2 test2  [{}]", ++param1);
+    }
+
+    public Clz2() {
+        log.info("clz2 constructor [{}]", ++param1);
+    }
+
+    @Override
+    public void test1() {
+        log.info("clz2 test1  [{}]", ++param1);
+    }
+
+}
